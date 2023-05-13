9f3d5b6bafe0add7b482f16016d969a3af39d330
L RMN <97253349+lrmn7@users.noreply.github.com>
2023-05-13 14:18:48 +0700

    Update FooterComponent.tsx

--- src/components/layouts/FooterComponent.tsx
+++ src/components/layouts/FooterComponent.tsx
@@ -1,23 +1,23 @@
-
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
 import { GiPayMoney } from "react-icons/gi";
 import { SiKofi } from "react-icons/si";
-import clsx from "@/lib/helpers/clsx";
+
+import clsxm from "@/lib/helpers/clsxm";
+
 import UnderlineLink from "../links/UnderlineLink";
-import UnstyledLink from "../links/UnstyledLink";
 
 const FooterComponent = () => {
   return (
-    <footer className={clsx("mt-24 w-full px-4 md:px-1 lg:mt-16")}>
+    <footer className={clsxm("mt-24 w-full px-4 md:px-1 lg:mt-16")}>
       <div
-        className={clsx(
+        className={clsxm(
           "flex flex-wrap items-center justify-center gap-2 sm:justify-between",
           "mx-auto my-4 max-w-4xl"
         )}
       >
         <div
-          className={clsx(
+          className={clsxm(
             "flex flex-wrap items-end justify-center",
             "gap-6 md:justify-start"
           )}
@@ -28,6 +28,7 @@ const FooterComponent = () => {
           >
             {new Date().getFullYear()} | L RMN
           </UnderlineLink>
+            />
         </div>
         <div className="inline-flex gap-6">
           <UnderlineLink href="https://ko-fi.com/lrmn7" className="gap-1">
